99589af33d8bb5da9384ffc17297cbe2d2332a8b
Juan Sebastian Bravo <js.bravo@uniandes.edu.co>
2021-07-08 00:37:23 -0500

    Semtag: feature versioning in main by default

--- build/app/backend/models/Asset.js
+++
@@ -1,1 +0,0 @@
-const{TRANSACTION_FAMILY,TRANSACTION_FAMILY_VERSION}=require("../utils/constants");class Asset{constructor({transactionFamily:t=TRANSACTION_FAMILY,transactionFamilyVersion:s=TRANSACTION_FAMILY_VERSION,inputs:i,outputs:a,payload:n}){this.transactionFamily=t,this.transactionFamilyVersion=s,this.inputs=i,this.outputs=a,this.payload=n}}module.exports=Asset;
